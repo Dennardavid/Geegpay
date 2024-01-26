34ab53f42a0abd96bf1512c3b08bd205142ee1bf
dennar david <dennardavid@gmail.com>
2024-01-26 04:58:14 +0100

    header works

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,17 @@ module.exports = {
         darkmode: "#34CAA5",
         sidebar: "#F7F8FA",
         sidebarborder: "#EBECF2",
+        headerboarder: "#E5EAEF",
+        textcolor: "#26282C",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
+      },
+      height: {
+        "6/7": "90%",
+      },
+      space: {
+        9: "50%",
       },
     },
   },
